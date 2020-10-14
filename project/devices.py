85fc5b08a2fecffc9bd214c96badbb0ed81b5fb1
ch4ot1k <charlesrlemonds@gmail.com>
2020-10-14 13:57:42 -0400

    test:  Test other syntax

--- project/devices.py
+++ project/devices.py
@@ -15,7 +15,7 @@ from project.pins import *
 
 class LED(Pin):
 	def __init__(self, pin):
-		Pin.__init__(self, pin, Pin.OUT, value = 0)
+		Pin(pin, Pin.OUT, value = 0).__init__(self)
 		# self.led = Pin(pin, Pin.OUT, value = 0)
 		self.led_timer = Timer(-1)
 		self.blink_timer = Timer(-1)
