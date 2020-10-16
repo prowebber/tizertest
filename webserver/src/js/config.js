f74193c71b85eafddf02dd8fe7a917ac495757f1
Steven Holdaway <webpurellc@gmail.com>
2020-10-16 11:00:13 -0400

    feat: Reset bag via splash screen

--- webserver/src/js/config.js
+++ webserver/src/js/config.js
@@ -71,17 +71,19 @@
       let bagPctMsg = bagLevel + "% remaining";
       let totalSprays = Math.round(mlSprayed / 0.8, 2);
       let spraysInBag = Math.round(500 / 0.8);
-      console.log('ml sprayed:' + mlSprayed);
-      console.log('bag level:' + bagLevel); // Set params for user's screen
+      /* Set params for user's screen */
+      // Usage info
 
       l.getDom('bag_pct_msg').innerHTML = bagPctMsg;
       l.getDom('bag_level').value = bagLevel;
       l.getDom('volume_sprayed').innerHTML = mlSprayed + 'ml';
       l.getDom('total_sprays').innerHTML = totalSprays;
-      l.getDom('c_total').innerHTML = spraysInBag;
+      l.getDom('c_total').innerHTML = spraysInBag; // Wifi info
+
       l.getDom('wifi_ssid').value = params['ssid'];
       l.getDom('wifi_pass').value = params['pass'];
-      l.getDom('has_wifi').innerHTML = params['has_wifi'] == '1' ? 'Connected to internet' : 'Not connected to internet';
+      l.getDom('has_wifi').innerHTML = params['has_wifi'] == '1' ? 'Connected to internet' : 'Not connected to internet'; // Other settings
+
       l.getDom('melody_status').value = params['melody'];
       l.getDom('spray_time_ms').value = parseInt(params['spray_time'], 10);
     }
@@ -117,6 +119,13 @@
               'cmd': 'reset_bag',
               'volume_ml': 500
             }));
+            let spraysInBag = Math.round(500 / 0.8);
+            l.getDom('bag_level').value = 100; // Reset bag level
+
+            l.getDom('volume_sprayed').innerHTML = '0ml'; // Volume sprayed
+
+            l.getDom('total_sprays').innerHTML = 0;
+            l.getDom('c_total').innerHTML = spraysInBag;
             l.getDom("output").value = "Reset Bag";
           }
         }
