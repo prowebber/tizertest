96009376379475d7ed4b4fb346329c6e04f006fb
ch4ot1k <charlesrlemonds@gmail.com>
2020-11-23 17:33:16 -0500

    refactor: re import setup since unrelated to bug

--- project/main.py
+++ project/main.py
@@ -6,7 +6,7 @@ from project.devices import RGBLED, LED, Button
 from project.tones import Speaker
 from project.rest_api import Rest
 from utime import sleep_ms, ticks_ms, ticks_diff
-# from main import setup
+from main import setup
 
 
 class Main:
